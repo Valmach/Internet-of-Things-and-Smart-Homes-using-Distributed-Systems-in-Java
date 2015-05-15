5da0ab5b32825084269ec1448c454b166d6f3fac
ashaik687 <ashaik@engin.umass.edu>
2015-05-15 09:32:09 -0400

    Project phase 3
    Internet of Things

---
+++ ProjectPhase3/allJars+scripts/run_LoadBalance_Consistency_Caches.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to allow the user to allow the user to take input manually and test for leader election algorithm and
+#time server
+
+gnome-terminal -x sh -c "java -jar GatewayServer.jar configips.csv; bash"&
+sleep 3
+gnome-terminal -x sh -c "java -jar GatewayServerReplica.jar configips.csv; bash"&
+sleep 3
+gnome-terminal -x sh -c "java -jar motionSensor.jar configips.csv; bash"&
+
+gnome-terminal -x sh -c "java -jar tempeSensor.jar configips.csv; bash"&
+
+gnome-terminal -x sh -c "java -jar DoorSensor.jar configips.csv; bash"&
+
+gnome-terminal -x sh -c "java -jar HeaterSmart.jar configips.csv; bash"&
+
+gnome-terminal -x sh -c "java -jar bulbSmart.jar configips.csv; bash"&
+
+gnome-terminal -x sh -c "java -jar BackendDatabase.jar configips.csv; bash"&
+
+
+
+
